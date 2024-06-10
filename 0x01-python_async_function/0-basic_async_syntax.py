1f0c49fc855259736bcc24f91068ae34b1c94e84
kaymacharia <kaymachariaa@gmail.com>
2024-06-10 14:46:42 +0300

    lockboxes FILE THAT is EXECUTABLE ;)

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
-'''Task 0 '''
+"""Defines the basic async syntax"""
 import asyncio
 import random
 
+
 async def wait_random(max_delay: int = 10) -> float:
-    """
-    Asynchronous coroutine that waits for a random delay between 0 and max_delay
-    """
-    wait = random.random() * max_delay  # Generate a random float between 0 and max_delay
-    await asyncio.sleep(wait)  # Wait for the delay
-    return wait  # Return the delay time
+    """Async syntax"""
+    wait = random.random() * max_delay
+    await asyncio.sleep(wait)
+    return wait

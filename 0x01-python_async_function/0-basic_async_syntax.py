b9ac0e42f1fed5f8adf9784cedaf0f9cc41b76b1
kaymacharia <kaymachariaa@gmail.com>
2024-06-10 14:09:50 +0300

    lockboxes FILE THAT is EXECUTABLE ;)

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -15,10 +15,3 @@ async def wait_random(max_delay: int = 10) -> float:
     delay = random.uniform(0, max_delay)  # Generate a random float between 0 and max_delay
     await asyncio.sleep(delay)  # Wait for the delay
     return delay  # Return the delay time
-
-# Example of how to run the coroutine
-if __name__ == "__main__":
-    # Run the coroutine and print the result
-    delay = asyncio.run(wait_random(10))
-    print(f"Waited for {delay} seconds")
-
